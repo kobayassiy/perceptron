277d34e6998842ef845022646fa83e8d0ffb8f1a
Koji Ono <kobayassiy.dev@gmail.com>
2014-04-03 00:42:26 +0900

    Modify perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'koji_ono'
-
 import csv
 import matplotlib.pyplot as plt
 
 LEARN_COEFFICIENT = 0.001
-#initial_weight_vec = [0,0,1]
-#initial_b = 3
-#training_data = [[1,1,1],[1,3,4],[-3,-2,-5]]
-#training_data_classes = [-1,1,-1]
+LOOP_MAX = 1000
 
 # 未知データの推測
 def predict(weight_vec, data_vec, b):
@@ -32,7 +27,7 @@ def train(weight_vec, data_vec, y, b):
         for i, weight in enumerate(weight_vec):
             weight_vec[i] = weight + LEARN_COEFFICIENT * y * data_vec[i]
         # バイアス項の更新
-        max_element = max(data_vec)
+        #max_element = max(data_vec)
         #b = b + LEARN_COEFFICIENT * y * max_element * max_element
     result = {
         'weight_vec' : weight_vec,
@@ -64,6 +59,7 @@ if __name__=='__main__':
     weight_vec = [0,1]
     b = 1
 
+    # 学習
     is_train_completed = False
     loop = 0
     while True:
@@ -71,7 +67,7 @@ if __name__=='__main__':
         if is_train_completed:
             print 'training finished! (converge)'
             break
-        if loop > 1000:
+        if loop > LOOP_MAX:
             print 'training finished! (not converge)'
             break
 
@@ -83,10 +79,38 @@ if __name__=='__main__':
                 weight_vec = result['weight_vec']
                 b = result['b']
                 updated_count += 1
+                #print updated_count
 
         print 'loop', loop, 'finished...', 'updated_count: ', updated_count
         is_train_completed = (updated_count == 0)
-    plt.scatter([1,2],[2,3],marker='o',color='g',s=2)
-    plt.show
 
+    # 学習結果
+    print 'weight : ' , weight_vec
+    print 'b : ' , b
+
+    # テスト
+    print '[5.0, 2.5]:' , predict(weight_vec, [5.0, 2.5], b)
+    print '[4.5, 4.8]:' , predict(weight_vec, [4.5, 4.8], b)
+
+    # 座標データの作成
+    x_1 = []
+    y_1=  []
+    x_2 = []
+    y_2 = []
+    for i, training_data_vec in enumerate(training_data):
+        if(training_data_label[i] == 1):
+            x_1.append(training_data_vec[0])
+            y_1.append(training_data_vec[1])
+        else:
+            x_2.append(training_data_vec[0])
+            y_2.append(training_data_vec[1])
+    plt.scatter(x_1,y_1, c='g')
+    plt.scatter(x_2,y_2, c='r')
+
+    # 識別面
+    x_fig=range(3,9)
+    y_fig=[-(weight_vec[0]/weight_vec[1])*xi-(b/weight_vec[1]) for xi in x_fig]
+    plt.plot(x_fig,y_fig)
+    
+    plt.show()
 
